9bb05abeb636b17a3e8bf3bfff096fb6a7999578
zerg959 <zerg959@yandex.ru>
2024-05-29 12:36:08 +0300

    CHANGE: admin panel

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
-Clone this repository using: git clone https://github.com/zerg959/flask-web-app.git
-Optional: Go to the project root directory and install the virtualenv library using pip and create a virtual environment. Run the following commands respectively:
-pip install virtualenv
-virtualenv venv
-source venv/bin/activate
-Download and install all dependencies in the requirements.txt using pip install -r requirements.txt
-Start Flask’s built-in web server: $ flask run
-Open your browser and view the app by opening the link http://127.0.0.1:5000/
-To change port reun: flask run -h localhost -p 3000
-You are free to view recent posts, register, login add new posts etc.
-To reinitialize sample database run $ python init_db.py from tmp folder.
-
-# SELECT user.id AS user_id, user.username AS user_username, user.email AS user_email, user.password AS user_password 
-# FROM user 
-# WHERE user.email = ?
-#  LIMIT ? OFFSET ?]
-# [parameters: ('123@gma.ru', 1, 0)]
+Clone this repository using: git clone https://github.com/zerg959/flask-wa-admin.git<br>
+Optional: Go to the project root directory and install the virtualenv library using pip and create a virtual environment.<br>
+Run the following commands respectively:<br>
+pip install virtualenv<br>
+virtualenv venv<br>
+source venv/bin/activate<br>
+Download and install all dependencies in the requirements.txt using pip install -r requirements.txt<br>
+Start Flask’s built-in web server: $ flask run<br>
+Open your browser and view the app by opening the link http://127.0.0.1:5000/<br>
+To change port reun: flask run -h localhost -p 3000<br>
+You are free to view recent posts, register, login add new posts etc.<br>
+To reinitialize sample database run $ python init_db.py from tmp folder.<br>
--- blog/__init__.py
+++ blog/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, url_for
 from flask_admin import Admin, expose, AdminIndexView, BaseView
-from flask_admin.contrib.sqla import ModelView
+# from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_required, current_user
+
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 
 login_manager = LoginManager()
@@ -17,49 +20,26 @@ bcrypt = Bcrypt()
 migrate = Migrate()
 
 
-class DashBoardView(AdminIndexView):
-    @login_required
-    @expose('/')
-    def admin_panel(self):
-        from blog.models import MyUser
-        all_users = MyUser.query.all()
-        # image_file = url_for('static',
-        #                      filename=f'profile_pics' + '/users/' + current_user.username + '/account_img/' +
-        #                               current_user.image_file)
-        return self.render('admin/index_admin1.html', all_users=all_users) # , image_file=image_file)
+class UserModelView(ModelView):
+    column_list = ('username', 'email')
+    form_columns = ('username', 'email')
 
-
-
-class AnyPageView(BaseView):
-    @expose('/')
-    def any_page(self):
-        return self.render('main/index.html')
-
-
-# class MyModelView(ModelView):
-#     def is_accsessiblr(self):
-#         return current_user.is_authenticated
-
-admin = Admin(name='Admin Board', template_mode='bootstrap3', index_view=DashBoardView(), endpoint='admin')
 
 def create_app():
+    from blog.models import MyUser, MyPost
     app = Flask(__name__)
-    # admin = Admin(app, name='Admin Board')
-    admin.init_app(app)
+    # admin = Admin(app)
+    admin = Admin(app, name='Admin Board', template_mode='bootstrap3')
+    admin.add_view(UserModelView(MyUser, db.session, name='Users'))
+    admin.add_view(ModelView(MyPost, db.session, name='Posts'))
+    # admin.init_app(app)
     app.config.from_pyfile('settings.py')
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db, render_as_batch=True)
 
-    from blog.models import MyUser, MyPost # Comment, Tag
-
-    admin.add_view(AnyPageView(name='На блог'))
-    admin.add_view(ModelView(MyUser, db.session, name='User name'))
-    admin.add_view(ModelView(MyPost, db.session, name='User Posts'))
-    # admin.add_view(ModelView(MyComment, db.session, name='Комментарии'))
-    # admin.add_view(ModelView(Tag, db.session, name='Теги'))
-
+ # Comment, Tag
     from blog.main.routes import main
     from blog.user.routes import users
     from blog.post.routes import posts
